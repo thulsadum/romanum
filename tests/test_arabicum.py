fb3bce4117d28984280f2f62211e5a181a780b9a
Sebastian Claus <sbstncls@gmail.com>
2021-07-06 22:57:25 +0200

    red(to_roman): more trivial tests

--- tests/test_arabicum.py
+++ tests/test_arabicum.py
@@ -11,3 +11,19 @@ def test_should_be_v():
 
 def test_should_be_x():
     assert to_roman(10) == "X"
+
+
+def test_should_be_l():
+    assert to_roman(50) == "L"
+
+
+def test_should_be_c():
+    assert to_roman(100) == "C"
+
+
+def test_should_be_d():
+    assert to_roman(500) == "D"
+
+
+def test_should_be_m():
+    assert to_roman(1000) == "M"
